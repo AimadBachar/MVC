2aa1fc04a5ddab79f4dd097f585c99e0c61b40ba
aimad bachar <aimadbacharpro@outlook.fr>
2021-11-17 16:49:08 +0100

    get request details

--- core/Request.php
+++ core/Request.php
@@ -6,7 +6,18 @@ class Request
 {
     public function getPath()
     {
-        $path = $_SERVER['REQUEST_URI'];
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
         $pos = strpos($path, '?');
+
+        if($pos === false) {
+            return $path;
+        }
+        return substr($path, 0, $pos);
+    }
+
+    public function getMethod()
+    {   
+        // pour faire correspondre 'GET' et 'get' strtolower on pourait utiliser 'GET' mais cela permet d'éviter de confondre avec les superglobale et les const
+        return strtolower($_SERVER['REQUEST_METHOD']) ?? 'get';
     }
 }
