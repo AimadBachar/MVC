ae75efbf055d936ee175d2f55674b798decc5640
aimad bachar <aimadbacharpro@outlook.fr>
2021-11-17 16:08:07 +0100

    use request application

--- core/Application.php
+++ core/Application.php
@@ -7,8 +7,12 @@ use App\Core\Router;
 class Application 
 {
     public Router $router;
+    public Request $request;
+
+
     public function __construct()
-    {
-        $this->router = new Router();
+    {   
+        $this->request = new Request();
+        $this->router = new Router($this->request);
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -2,15 +2,25 @@
 
 namespace App\Core;
 
+use App\Core\Request;
 
 class Router 
 {   
-    protected $routes = [
-        '/' => '.....',
-        '/about' => '.....',
-    ];
+    protected $routes = [ ];
+    public Request $request;
+
+    public function __construct()
+    {
+        $this->request = new Request();
+    }
+
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+         $this->request->getPath();
     }
 }
