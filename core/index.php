c0a7ae8884c3c53961dab665211322f4f3c00b3d
aimad bachar <aimadbacharpro@outlook.fr>
2021-11-17 16:02:19 +0100

    init index

---
+++ core/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\Application;
+
+$app = new Application();
+
+$app->router->get('/', function () {
+    echo 'Hello world!';
+});
+
+$app->router->get('/contact', function () {
+    echo 'Contact Page!';
+});
+
+$app->run();
